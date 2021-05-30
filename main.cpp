37be4d2ecf6a356e1e1e6ab4402ccd823df7b36b
kaki <suka6411@gmail.com>
2021-05-30 21:23:29 +0900

    feat: コマンドラインパーサを導入した

--- main.cpp
+++ main.cpp
@@ -4,8 +4,33 @@
 #include <system_error>
 #include <vector>
 #include "cat.hpp"
+#include "cxxopts.hpp"
 
 int main(int argc, char *argv[]) {
+    cxxopts::Options options("cat", "Concatenate FILE(s) to standard output.");
+
+    options.add_options()
+        ("n,number", "number all output line")
+        ("help", "print this message and exit")
+    ;
+
+    cxxopts::ParseResult result;
+    try {
+        result = options.parse(argc, argv);
+    } catch (cxxopts::OptionException e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(EXIT_SUCCESS);
+    }
+
+    options.custom_help("[OPTION]... [FILE]...");
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        std::exit(EXIT_SUCCESS);
+    }
+
+    std::cout << result.unmatched()[0] << std::endl;
+
     auto cat = cat::Cat();
     try {
         if (argc == 1) {
